928ab10fb91af9d602ca30891129f987a1c1c095
Darren Churchill <darrenmchurchill@gmail.com>
2024-03-09 13:01:10 -0800

    Add 3.5 SEAT Approach beforeEach() setup

--- lesson_3/jest_testing/car.test.js
+++ lesson_3/jest_testing/car.test.js
@@ -4,50 +4,45 @@
 const Car = require("./car");
 
 describe("The Car class", () => {
+  let car;
+  beforeEach(() => {
+    car = new Car();
+  });
+
   test("has four wheels", () => {
-    let car = new Car();
     expect(car.wheels).toBe(4);
   });
 
   test("car wheels NOT undefined", () => {
-    let car = new Car();
     expect(car.wheels).not.toBeUndefined();
   });
 
   test("two new cars are equal objects", () => {
-    let car1 = new Car();
     let car2 = new Car();
-
-    expect(car1).toEqual(car2);
+    expect(car).toEqual(car2);
   });
 
   test("does not have doors", () => {
-    let car = new Car();
     expect(car.doors).toBeUndefined();
   });
 
   test("raises an error when called drive on it", () => {
-    let car = new Car();
     expect(() => car.drive()).toThrow();
   });
 
   test("raises a TypeError when called drive on it", () => {
-    let car = new Car();
     expect(() => car.drive()).toThrow(TypeError);
   });
 
   test("a new car has no mileage info", () => {
-    let car = new Car();
     expect(car.mileageInfo).toBeNull();
   });
 
   test("wheels is truthy", () => {
-    let car = new Car();
     expect(car.wheels).toBeTruthy();
   });
 
   test("array contains car", () => {
-    let car = new Car();
     let arr = [1, 2, 3];
     arr.push(car);
 
